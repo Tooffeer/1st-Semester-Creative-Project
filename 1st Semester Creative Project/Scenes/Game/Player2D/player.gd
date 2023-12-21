65859bf6a72c2cc1319266fe054266232204969e
Tooffeer <138416859+Tooffeer@users.noreply.github.com>
2023-12-21 15:48:03 -0800

    Coyote Jump
    Need to organize code and scenes.

---
+++ 1st Semester Creative Project/Scenes/Game/Player2D/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+# Jumping
+const JUMP_VELOCITY = -400.0
+var canJump : bool = true
+var coyote_timer = 0
+var coyoteTime = 0.23
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Apply gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# Determine coyote jump
+	if is_on_floor():
+		canJump = true
+		coyote_timer = 0
+	else:
+		coyote_timer += delta
+		if coyote_timer > coyoteTime:
+			canJump = false
+	
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump"):
+		if canJump == true:
+			velocity.y = JUMP_VELOCITY
+			canJump = false
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
---
+++ 1st Semester Creative Project/Scenes/Main/main.gd
@@ -0,0 +1,30 @@
+extends Node
+
+# Preload scenes
+var gameScene = preload("res://Scenes/Game/game.tscn")
+var level_1 = preload("res://Scenes/Game/Levels/Level_1/level_1.tscn")
+var player = preload("res://Scenes/Game/Player2D/player.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _on_play_pressed():
+	loadScene()
+	$Title.hide()
+
+# Organize this later
+func loadScene():
+	# Get game scene
+	var instance = gameScene.instantiate()
+	player = player.instantiate()
+	level_1 = level_1.instantiate()
+	# Add scene to the tree
+	self.add_child(instance)
+	instance.add_child(level_1)
+	instance.add_child(player)
