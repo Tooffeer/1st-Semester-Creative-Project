8ef88c276a2de1c5eedf54dab1947ef10624ae70
Tooffeer <138416859+Tooffeer@users.noreply.github.com>
2024-01-24 13:29:15 -0800

    Grendel Knockback

--- 1st Semester Creative Project/Scenes/Game/Enemies/Grendel/grendel.gd
+++ 1st Semester Creative Project/Scenes/Game/Enemies/Grendel/grendel.gd
@@ -8,7 +8,9 @@ extends CharacterBody2D
 
 # Movement
 var direction = 0
-const speed = 300.0
+@export var speed = 350.0
+var charging = false
+var attacking = false
 
 var player
 
@@ -22,33 +24,51 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	
 	if direction:
-		velocity.x = speed * direction * delta * 20
+		velocity.x = move_toward(velocity.x, direction * speed, delta * 500)
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, delta * 400)
 	
-	# Would add knockback here
+	# Detect if bumping into wall
+	if is_on_wall() and charging:
+		var knockback= Vector2(-direction * speed, -350)
+		velocity += knockback
+		
+		charging = false
+		direction = 0
+		
+		print(direction)
 	
 	move_and_slide()
 	
-	if velocity.x > 0:
-		sprite.flip_h = true
-	elif velocity.x < 0:
-		sprite.flip_h = false
-	
+	animate()
 	
 	if health <= 0:
 		die()
 
 func _on_player_detection_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and attacking == false:
 		player = body
+		attacking = false
 		
 		charge()
 
 func charge():
+	charging = true
+	
 	# Determine if player is on the right or left side of Grendel
 	if ((position.x - player.position.x) > 0):
 		direction = -1
 	elif ((position.x - player.position.x) < 0):
 		direction = 1
+	
+	print("Charging")
+
+func animate():
+	# Flip sprite in the direction of movement. 
+	if velocity.x > 0:
+		sprite.flip_h = true
+	elif velocity.x < 0:
+		sprite.flip_h = false
 
 func die():
 	direction = 0
